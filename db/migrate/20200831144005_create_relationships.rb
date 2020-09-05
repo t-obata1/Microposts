a2da2aef53f6dd7a1c21086a7c84e7d9784b2298
t.obata <ec2-user@ip-172-31-33-234.ec2.internal>
2020-09-05 09:56:22 +0000

    heroku2

--- db/migrate/20200831144005_create_relationships.rb
+++ db/migrate/20200831144005_create_relationships.rb
@@ -5,7 +5,7 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
       t.references :follow, foreign_key: {to_table: :users } #usersテーブルに紐づけ
 
       t.timestamps
-      t.index [:user_id, :follow_id], unique: true　#フォロー側とされる側の重複を回避
+      t.index [:user_id, :follow_id], unique: true #フォロー側とされる側の重複を回避
       end
   end
 end
